19b36bb9f8cfb5fbe6dc9188215938dffe91430c
Michael Hunger <github@jexp.de>
2011-12-19 11:58:03 +0100

    can now also handle json string and hash post data

--- cypher-endpoint.rb
+++ cypher-endpoint.rb
@@ -5,7 +5,12 @@ require 'json'
 rest = RestClient::Resource.new(ENV['NEO4J_URL'])
 
 post '/raw-cypher' do
-    data = {:query=>request.body.read }
+    data = request.body.read;
+    begin
+      data = JSON.parse(data)
+    rescue
+    end 
+    data = {:query => data } unless data.kind_of?(Hash)
     rest["/db/data/ext/CypherPlugin/graphdb/execute_query"].post data.to_json, 
                  {:accept=>"application/json",:content_type=>"application/json"}
 end
